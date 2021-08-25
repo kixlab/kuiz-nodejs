42df9b3621fd70db19d191173af0c1590e288f21
Kijoon <igreen0485@gmail.com>
2021-08-25 17:15:12 +0900

    restructurized router

--- src/routes/class.js
+++ src/routes/class.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
 // import * as authControllers from '../middlewares/auth';
-var createClassMiddleware = require('../middlewares/class/createClassMiddleware')
-var joinClassMiddleware = require('../middlewares/class/joinClassMiddleware')
+//check auth related middleware
+var checkInClass = require('../middlewares/class/checkInClassMiddleware')
+var checkIsUser = require('../middlewares/auth/checkIsUserMiddleware')
+//class related middleware
+var createClass = require('../middlewares/class/createClassMiddleware')
+var joinClass = require('../middlewares/class/joinClassMiddleware')
+var loadClass = require('../middlewares/class/loadClassMiddleware')
+//question related middleware
+var createQuestion = require("../middlewares/class/question/createQuestionMiddleware")
+var deleteQuestion = require("../middlewares/class/question/deleteQuestionMiddleware")
+var modifyQuestion = require("../middlewares/class/question/modifyQuestionMiddleware")
+var loadQuestion = require("../middlewares/class/question/loadQuestionMiddleware")
+//feedback related middleware
+var createFeedback = require("../middlewares/class/feedback/createFeedbackMiddleware")
+var deleteFeedback = require("../middlewares/class/feedback/deleteFeedbackMiddleware")
+var loadFeedback = require("../middlewares/class/feedback/loadFeedbackMiddleware")
 
-// router.get ('/', authControllers.authCheck);
-router.post('/create',createClassMiddleware)
-router.post('/join',joinClassMiddleware)
-// router.get ('/login', authControllers.login);
-// router.post ('/login/callback', authControllers.loginCallback);
-// router.get ('/logout', authControllers.logout);
-// router.get ('/unregister', authControllers.unregister);
+router.post('/create', checkIsUser, createClass)
+router.post('/join', checkIsUser, joinClass)
+router.get('/load', checkIsUser, loadClass)
+
+router.get('/question/load', checkIsUser, checkInClass, loadQuestion)
+router.post('/question/create',checkIsUser, checkInClass, createQuestion);
+router.post('/question/delete',checkIsUser, checkInClass, deleteQuestion);
+router.post('/question/modify',checkIsUser, checkInClass, modifyQuestion);
+
+router.get('/feedback/load',checkIsUser, loadFeedback)
+router.post('/feedback/create', checkIsUser, checkInClass, createFeedback);
+router.post('/feedback/delete', checkIsUser, checkInClass, deleteFeedback)
 
 module.exports = router;
