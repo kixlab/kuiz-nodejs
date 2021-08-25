0b6cd34d41fdce9a809d920549f22dfac262b08b
Kijoon <igreen0485@gmail.com>
2021-08-25 20:30:53 +0900

    added loadClassMiddleware

---
+++ src/middlewares/class/loadClassMiddleware.js
@@ -0,0 +1,37 @@
+const User = require('../../db/models/user');
+const Class = require('../../db/models/class')
+
+const loadClassMiddleware = (req,res,next) => {
+    const classid = req.query.classId;
+    const email = req.body.email;
+
+    const findUser = (email) =>{
+        return new Promise((res,rej)=>{
+            User.findOne({email:email},(err,data)=>{
+                if(err) throw err;
+                else{
+                    resolve(data.classes)
+                }
+            })
+        })
+    }
+    const checkClass = (classes) =>{
+        return new Promise((res,rej)=>{
+            Class.find({ classId : {$in:classes}},(err,data)=>{
+                if(err) throw err;
+                else{
+                    resolve(data)
+                }
+            })
+        })
+    }
+
+    findUser(email)
+    .then(classes => checkClass(classes))
+    .then(classinfo => res.json({classInfo : classinfo, msg:"success", success:true}))
+    .catch(err => console.log("err",err))
+
+
+}
+
+module.exports = loadClassMiddleware;
