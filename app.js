afc824a416246a373ba31816c457008cc35975d7
beardoggo <2ngddo@gmail.com>
2021-11-23 00:28:50 +0900

    Removed && in yarn start script and it's now working!

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+console.log(process.env.NODE_ENV);
 const PORT = process.env.NODE_ENV === "development" ? 4000 : 5000;
 
 const options = {
