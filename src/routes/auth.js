9dba3d8db98f9edcb7738948e6cb27ef3174cab7
beardoggo <2ngddo@gmail.com>
2021-11-24 15:59:06 +0900

    Merge

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // import * as authControllers from '../middlewares/auth';
-var registerMiddleware = require('../middlewares/auth/registerMiddleware')
+var registerMiddleware = require("../middlewares/auth/registerMiddleware");
+var changeSetting = require("../middlewares/auth/changeSettingMiddleware");
 // router.get ('/', authControllers.authCheck);
-router.post('/register',registerMiddleware)
+router.post("/register", registerMiddleware);
+router.post("/set", changeSetting);
 
 // router.get ('/login', authControllers.login);
 // router.post ('/login/callback', authControllers.loginCallback);
